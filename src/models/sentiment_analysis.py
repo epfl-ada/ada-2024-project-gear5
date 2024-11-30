6777b8aadf54f0b769c95d701d31a06d71f14910
mahmouddokmak <mahmoud.dokmak@epfl.ch>
2024-11-30 12:43:19 +0100

    reworked sa

--- src/models/sentiment_analysis.py
+++ src/models/sentiment_analysis.py
@@ -1,17 +1,28 @@
 import pandas as pd
-from transformers import pipeline
+from transformers import pipeline, AutoTokenizer, AutoModelForSequenceClassification
 
-def perform_sentiment_analysis(reviews, sa_column):
-    #if invalid column name, we stop
+def sentiment_analysis(reviews, sa_column):
+    # if invalid column name, we stop
     if sa_column not in reviews.columns:
         raise ValueError(f"The column{sa_column}doesn't exist")
+    # Loading a Pre-Trained Model from HuggingFace Hub
+    model_name = "cardiffnlp/twitter-roberta-base-sentiment"
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForSequenceClassification.from_pretrained(model_name)
     #ensure that the reviews are not too long.
     reviews[sa_column] = [
-        reviews[sa_column].iloc[i][:500] if isinstance(reviews[sa_column].iloc[i], str) else reviews[sa_column].iloc[i]
+        reviews[sa_column].iloc[i][:512] if isinstance(reviews[sa_column].iloc[i], str) else reviews[sa_column].iloc[i]
         for i in range(reviews.shape[0])
     ]
-    #perform sentiment analysis
-    sa_analyzer = pipeline("sentiment-analysis", model="cardiffnlp/twitter-roberta-base-sentiment")
-    reviews["sentiment"] = reviews[sa_column].apply(lambda x: sa_analyzer(x)[0]['label'] if pd.notnull(x) else None)
-    reviews["sentiment_score"] = reviews[sa_column].apply(lambda x: sa_analyzer(x)[0]['score'] if pd.notnull(x) else None)
+    #intialize the sentiment analysis classifier
+    sa_classifier = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
+    sa = apply_sentiment_analysis(sa_classifier, reviews, sa_column)
+    return sa
+
+def apply_sentiment_analysis(sa_classifier, reviews, sa_column):
+    reviews["sentiment_analysis"] = reviews[sa_column].apply(lambda x: sa_classifier(x)[0] if pd.notnull(x) else None)
+    reviews["sentiment"] = reviews["sentiment_analysis"].apply(lambda x: x['label'] if pd.notnull(x) else None)
+    reviews["sentiment_score"] = reviews["sentiment_analysis"].apply(lambda x: x['score'] if pd.notnull(x) else None)
+    reviews.drop(columns=["sentiment_analysis"], inplace=True)
     return reviews
+
