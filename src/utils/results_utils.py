fb4a26ab2f6659d18135e3f823bed0504643fa7b
loXpac <94895935+loXpac@users.noreply.github.com>
2024-12-20 19:46:55 +0100

    Update results_utils.py

--- src/utils/results_utils.py
+++ src/utils/results_utils.py
@@ -4,7 +4,7 @@ from sklearn.preprocessing import MinMaxScaler
 
 # function to print the size of the dataframe
 def print_df_size(name, df):
-	print(name, f"- shape: {df.shape} --> {df.shape[0]} rows and {df.shape[1]} columns")
+    print(name, f"- shape: {df.shape} --> {df.shape[0]} rows and {df.shape[1]} columns")
 
 # We don't have information about inflation before 1913, so we will apply the 1913 inflation rates to the oldest movies
 def inflation_date_constraint(date):
@@ -14,6 +14,19 @@ def inflation_date_constraint(date):
 
 # Function to clean the data after the merge of CMU Movie Summary Corpus with TMDB
 def merge_cmu_tmdb_clean_columns(df_merged):
+    """
+    Cleans the merged DataFrame by dropping unnecessary columns.
+    This function takes a merged DataFrame containing data from CMU and TMDB,
+    and removes a predefined list of columns that are not needed for further analysis.
+    
+    Parameters :
+        df_merged (pd.DataFrame) : 
+            The merged DataFrame containing data from CMU and TMDB.
+    Returns :
+        pd.DataFrame : 
+            A DataFrame with the specified columns removed.
+    """
+    
     columns_to_drop = [
     "wikipedia_movie_id",
     "movie_runtime",
@@ -41,6 +54,24 @@ def merge_cmu_tmdb_clean_columns(df_merged):
 
 # Function to remove dictionaries from the columns
 def merge_cmu_tmdb_remove_dictionaries(full_reduced):
+    """
+    Cleans and merges movie country and genre information from different sources in the given DataFrame.
+    This function performs the following steps:
+    1. Cleans the 'movie_countries' and 'movie_genres' columns by evaluating their string representations of dictionaries
+       and extracting their values.
+    2. Converts the cleaned lists into comma-separated strings, replacing empty lists with NaN.
+    3. Combines the cleaned 'movie_countries' and 'movie_genres' columns with 'production_countries' and 'genres' columns,
+       respectively, prioritizing the cleaned columns.
+    4. Drops the original and intermediate columns used in the cleaning process.
+    
+    Parameters :
+        full_reduced (pd.DataFrame) : 
+            A DataFrame containing movie information with columns 'movie_countries', 'movie_genres', 'production_countries', and 'genres'.
+    Returns :
+        pd.DataFrame : 
+            The modified DataFrame with cleaned and merged movie country and genre information.
+    """
+    
     full_reduced["movie_countries_cleaned"] = full_reduced["movie_countries"].apply(lambda x: list(dict(eval(x)).values()))
     full_reduced["movie_genres_cleaned"] = full_reduced["movie_genres"].apply(lambda x: list(dict(eval(x)).values()))
     full_reduced["movie_countries_cleaned"] = full_reduced["movie_countries_cleaned"].apply(lambda x: np.nan if x == [] else ', '.join(map(str, x)) if isinstance(x, list) else x)
@@ -54,6 +85,24 @@ def merge_cmu_tmdb_remove_dictionaries(full_reduced):
 
 # Combine the redundant columns and ensure that we keep the maximum amount of information
 def merge_cmu_tmdb_combine_columns(full_reduced):
+    """
+    Merges and combines specific columns in the given DataFrame.
+    This function performs the following operations:
+    1. Combines 'release_date' and 'movie_release_date' columns into a new column 'combined_release_date'.
+    2. Drops the original 'release_date' and 'movie_release_date' columns.
+    3. Converts 'combined_release_date' to datetime format, coercing errors.
+    4. Replaces zero values in 'movie_box_office_revenue' and 'revenue' columns with NaN.
+    5. Combines 'movie_box_office_revenue' and 'revenue' columns into a new column 'Box_Office'.
+    6. Drops the original 'movie_box_office_revenue', 'revenue', 'movie_box_office_revenue_nan', and 'revenue_nan' columns.
+    
+    Parameters:
+        full_reduced (pd.DataFrame) : 
+            The DataFrame containing the columns to be merged and combined.
+    Returns:
+        pd.DataFrame : 
+            The modified DataFrame with combined columns.
+    """
+    
     full_reduced['combined_release_date'] = full_reduced['release_date'].combine_first(full_reduced['movie_release_date'])
     full_reduced.drop(columns=['movie_release_date', 'release_date'], inplace=True)
     full_reduced['combined_release_date'] = pd.to_datetime(full_reduced['combined_release_date'], errors='coerce')
@@ -64,6 +113,20 @@ def merge_cmu_tmdb_combine_columns(full_reduced):
     return full_reduced
 
 def combine_ratings(data_box_office):
+    """
+    Combines and processes rating columns in the given DataFrame.
+    This function takes a DataFrame containing box office data with columns 'avgRating' and 'vote_avg'.
+    It combines these columns to create a new rating column, 'rating', which is the average of the two.
+    Missing values in 'avgRating' and 'vote_avg' are handled by filling them with zeros.
+    
+    Parameters:
+        data_box_office (pd.DataFrame) : 
+            A DataFrame containing box office data with 'avgRating' and 'vote_avg' columns.
+    Returns:
+        pd.DataFrame : 
+            The modified DataFrame with a new 'rating' column and without the original 'avgRating' and 'vote_avg' columns.
+    """
+    
     data_box_office['avgRating_safe'] = data_box_office['avgRating'].combine_first(data_box_office['vote_avg'])
     data_box_office['vote_safe'] = data_box_office['vote_avg'].combine_first(data_box_office['avgRating'])
     data_box_office['avgRating_safe'].fillna(0, inplace=True)
@@ -73,8 +136,28 @@ def combine_ratings(data_box_office):
     return data_box_office
 
 def success(data_box_office, popularity, box_office, sa_score, rating):
+    """
+    Calculate the success score for movies based on various metrics and scales it between 0 and 5.
+    
+    Parameters:
+        data_box_office (pd.DataFrame) : 
+            DataFrame containing movie data including box office information.
+        popularity (str) : 
+            Column name in data_box_office representing the popularity metric.
+        box_office (str) : 
+            Column name in data_box_office representing the box office metric.
+        sa_score (str) : 
+            Column name in data_box_office representing the sentiment analysis score.
+        rating (str) : 
+            Column name in data_box_office representing the movie rating.
+    Returns:
+        pd.DataFrame : 
+            DataFrame with additional columns 'success_score' and 'success_metric_scaled', 
+            sorted by 'success_metric_scaled' in descending order.
+    """
+
     weights = {
-         popularity: 10,
+        popularity: 10,
         box_office: 8,
         sa_score: 3,
         rating: 3
