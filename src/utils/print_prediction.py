d1b5d8fc28dfef23d5cb975e7c0e0ef9c1a50839
loXpac <94895935+loXpac@users.noreply.github.com>
2024-12-20 19:40:23 +0100

    docstring

--- src/utils/print_prediction.py
+++ src/utils/print_prediction.py
@@ -1,15 +1,24 @@
 import matplotlib.pyplot as plt
 
 def plot_results(train, test, predicted_values, title):
+    """Plot the results of a prediction.
+
+    Parameters
+    ----------
+    train : list or array-like
+        The training data.
+    test : list or array-like
+        The test data.
+    predicted_values : list or array-like
+        The predicted values.
+    title : str
+        The title of the plot.
+    """
     plt.figure(figsize=(10, 5))
-
     plt.plot(range(len(train)), train, label='Train Data')
-
     plt.plot(range(len(train), len(train) + len(test)), test, label='Test Data')
-
     prediction_range = range(len(train), len(train) + len(predicted_values))
     plt.plot(prediction_range, predicted_values, label='Predicted', linestyle='--')
-
     plt.title(title)
     plt.legend()
     plt.show()
